399784ee39c02bdc14979f98aba280044ab2dd2e
Kyrre Havik <kyrremann@gmail.com>
2022-08-02 13:28:51 +0200

    feat: kjørt rubocop

--- poc.rb
+++ poc.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'date'
 require 'json'
 require 'pp'
@@ -10,48 +12,48 @@ require 'time'
 
 def get_periode
   {
-    "ident" => "E152435",
-    "satser" => {
-      "lørsøn" => 55,
-      "0620" =>  10,
-      "2006" => 20,
-      "utvidet" => 15
+    "ident": 'E152435',
+    "satser": {
+      "lørsøn": 55,
+      "0620": 10,
+      "2006": 20,
+      "utvidet": 15
     },
-    "periode" => {
-      "14.03.2022" => {
-        "fra" => "0000",
-        "til" => "2400",
-        "helligdag" => false
+    "periodes": {
+      "14.03.2022": {
+        "fra": '0000',
+        "til": '2400',
+        "helligdag": false
       },
-      "15.03.2022" => {
-        "fra" => "0000",
-        "til" => "2400",
-        "helligdag" => false
-      }, 
-      "16.03.2022" => {
-        "fra" => "0000",
-        "til" => "2400",
-        "helligdag" => false
+      "15.03.2022": {
+        "fra": '0000',
+        "til": '2400',
+        "helligdag": false
       },
-      "17.03.2022" => {
-        "fra" => "0000",
-        "til" => "2400",
-        "helligdag" => true
+      "16.03.2022": {
+        "fra": '0000',
+        "til": '2400',
+        "helligdag": false
       },
-      "18.03.2022" => {
-        "fra" => "0000",
-        "til" => "2400",
-        "helligdag" => false
+      "17.03.2022": {
+        "fra": '0000',
+        "til": '2400',
+        "helligdag": true
       },
-      "19.03.2022" => {
-        "fra" => "0000",
-        "til" => "2400",
-        "helligdag" => false
+      "18.03.2022": {
+        "fra": '0000',
+        "til": '2400',
+        "helligdag": false
       },
-      "20.03.2022" => {
-        "fra" => "0000",
-        "til" => "2400",
-        "helligdag" => false
+      "19.03.2022": {
+        "fra": '0000',
+        "til": '2400',
+        "helligdag": false
+      },
+      "20.03.2022": {
+        "fra": '0000',
+        "til": '2400',
+        "helligdag": false
       }
     }
   }
@@ -59,39 +61,39 @@ end
 
 def get_minwintid
   {
-    "14.03.2022" => ["07:15-15:33"],
-    "15.03.2022" => ["07:15-15:57"],
-    "16.03.2022" => ["01:00-03:00", "07:31-15:33"],
-    "17.03.2022" => ["07:55-16:10"],
-    "18.03.2022" => ["07:30-16:19"],
-    "19.03.2022" => [],
-    "20.03.2022" => []
+    "14.03.2022": ['07:15-15:33'],
+    "15.03.2022": ['07:15-15:57'],
+    "16.03.2022": ['01:00-03:00', '07:31-15:33'],
+    "17.03.2022": ['07:55-16:10'],
+    "18.03.2022": ['07:30-16:19'],
+    "19.03.2022": [],
+    "20.03.2022": []
   }
 end
 
 def get_agresso
   # koststed, personalleder, årslønn
-  ["123321", "Knut Mork", "500_000"]
+  ['123321', 'Knut Mork', '500_000']
 end
 
 def time_to_minutes(time)
   t = Time.parse(time)
-  t.hour*60 + t.min
+  t.hour * 60 + t.min
 end
 
 def ranges_overlap?(rA, rB)
-  rB.begin <= rA.end && rA.begin <= rB.end 
+  rB.begin <= rA.end && rA.begin <= rB.end
 end
 
-def countRangeOverlap(rA, rB)
+def count_range_overlap(rA, rB)
   return rB.count if rA.cover?(rB)
-  
+
   rangeStart = rB.begin < rA.begin ? rA.begin : rB.begin
   rangeEnd = rB.end > rA.end ? rA.end : rB.end
-  return (rangeStart...rangeEnd).count
+  (rangeStart...rangeEnd).count
 end
 
-def parse_periode(periodes, timecard, salary)
+def parse_periode(periodes, timecard)
   minutes = {}
 
   # klokkeslett blir gjort om til minutter
@@ -101,7 +103,6 @@ def parse_periode(periodes, timecard, salary)
   preWorkRange = (360...420) # 06-07
   postWorkRange = (1020...1200) # 17-20
   weekendRange = (0...1440) # 00-24
-
 
   # check for summertime/winterTime
   today = Date.today
@@ -117,50 +118,50 @@ def parse_periode(periodes, timecard, salary)
     nightTimeRange = (0...300)
   end
 
-  periodes["periode"].each do |day, periode|
-    date = Date.parse(day)
+  periodes.each do |day, periode|
+    date = Date.parse(day.to_s)
 
     minutes[day] = {
-      "dayMinutes" => dayTimeRange.count,
-      "nightMinutes" => nightTimeRange.count + eveningTimeRange.count
+      "dayMinutes": dayTimeRange.count,
+      "nightMinutes": nightTimeRange.count + eveningTimeRange.count
     }
 
-    if periode["helligdag"] || date.saturday? || date.sunday?
-      minutes[day]["weekendMinutes"] = weekendRange.count
-      minutes[day]["helgetillegg"] = true
+    if periode[:helligdag] || date.saturday? || date.sunday?
+      minutes[day][:weekendMinutes] = weekendRange.count
+      minutes[day][:helgetillegg] = true
     else
-      minutes[day]["workMinutes"] = preWorkRange.count + postWorkRange.count
-      minutes[day]["helgetillegg"] = false
+      minutes[day][:workMinutes] = preWorkRange.count + postWorkRange.count
+      minutes[day][:helgetillegg] = false
     end
 
     workingHours = timecard[day]
-    # todo: støtte forskjellige vaktplaner, denne koden funker kun for døgnvakt
+    # TODO: støtte forskjellige vaktplaner, denne koden funker kun for døgnvakt
     workingHours.each do |values|
       from, to = values.split('-')
       workRange = (time_to_minutes(from)...time_to_minutes(to))
 
       if ranges_overlap?(nightTimeRange, workRange)
-        minutes[day]["nightMinutes"] -= countRangeOverlap(nightTimeRange, workRange)
+        minutes[day][:nightMinutes] -= count_range_overlap(nightTimeRange, workRange)
       end
 
       if ranges_overlap?(eveningTimeRange, workRange)
-        minutes[day]["nightMinutes"] -= countRangeOverlap(eveningTimeRange, workRange)
+        minutes[day][:nightMinutes] -= count_range_overlap(eveningTimeRange, workRange)
       end
 
       if ranges_overlap?(dayTimeRange, workRange)
-        minutes[day]["dayMinutes"] -= countRangeOverlap(dayTimeRange, workRange)
+        minutes[day][:dayMinutes] -= count_range_overlap(dayTimeRange, workRange)
       end
 
       # ikke i helger eller helligdager!
-      if periode["helligdag"] || date.saturday? || date.sunday?
-        minutes[day]["weekendMinutes"] -= workRange.count
+      if periode[:helligdag] || date.saturday? || date.sunday?
+        minutes[day][:weekendMinutes] -= workRange.count
       else
         if ranges_overlap?(preWorkRange, workRange)
-          minutes[day]["workMinutes"] -= countRangeOverlap(preWorkRange, workRange)
+          minutes[day][:workMinutes] -= count_range_overlap(preWorkRange, workRange)
         end
 
         if ranges_overlap?(postWorkRange, workRange)
-          minutes[day]["workMinutes"] -= countRangeOverlap(postWorkRange, workRange)
+          minutes[day][:workMinutes] -= count_range_overlap(postWorkRange, workRange)
         end
       end
     end
@@ -170,39 +171,40 @@ def parse_periode(periodes, timecard, salary)
 end
 
 def calculate_compensation(worked)
-  p "Compensation"
-  dayHours = (worked.sum {|_, values| values["dayMinutes"]}/60.0).round
-  nightHours = (worked.sum {|_, values| values["nightMinutes"]}/60.0).round
-  workHours = (worked.sum {|_, values| values["workMinutes"] || 0}/60.0).round
-  weekendHours = (worked.sum {|_, values| values["weekendMinutes"] || 0}/60.0).round
+  p 'Compensation'
+  dayHours = (worked.sum { |_, values| values[:dayMinutes] } / 60.0).round
+  nightHours = (worked.sum { |_, values| values[:nightMinutes] } / 60.0).round
+  workHours = (worked.sum { |_, values| values[:workMinutes] || 0 } / 60.0).round
+  weekendHours = (worked.sum { |_, values| values[:weekendMinutes] || 0 } / 60.0).round
   p dayHours, nightHours, workHours, weekendHours
-  p dayCompensation = dayHours*10.0
-  p nightCompensation = nightHours*20.0
-  p workCompensation = (workHours*15.0)/5
-  p weekendCompensation = (weekendHours*55.0)/5
+  p dayCompensation = dayHours * 10.0
+  p nightCompensation = nightHours * 20.0
+  p workCompensation = (workHours * 15.0) / 5
+  p weekendCompensation = (weekendHours * 55.0) / 5
 
-  print "Total: "
+  print 'Total: '
   dayCompensation + nightCompensation + workCompensation + weekendCompensation
 end
 
 def calculate_overtime(worked, salary)
-  p "Overtime"
-  overtimeWeekendHours = ((worked.sum {|_, values| values["helgetillegg"] ? values["dayMinutes"] : 0}/60.0) + (worked.sum {|_, values| values["helgetillegg"] ? values["nightMinutes"] : 0}/60.0)).round
-  overtimeDayWorkHours = (worked.sum {|_, values| values["helgetillegg"] ? 0 : values["dayMinutes"]}/60.0).round
-  overtimeNightHours = (worked.sum {|_, values| values["helgetillegg"] ? 0 : values["nightMinutes"]}/60.0).round
+  p 'Overtime'
+  overtimeWeekendHours = ((worked.sum {|_, values| values[:helgetillegg] ? values[:dayMinutes] : 0} / 60.0) +
+                          (worked.sum {|_, values| values[:helgetillegg] ? values[:nightMinutes] : 0} / 60.0)).round
+  overtimeDayWorkHours = (worked.sum { |_, values| values[:helgetillegg] ? 0 : values[:dayMinutes] } / 60.0).round
+  overtimeNightHours = (worked.sum { |_, values| values[:helgetillegg] ? 0 : values[:nightMinutes] } / 60.0).round
   p overtimeWeekendHours, overtimeDayWorkHours, overtimeNightHours
   # =IF(B6="Vanlig";(AZ6*A$33+(AX6+AY6)*A$31)/5;(AX6+AY6+AZ6)*A$31/5)
   # b6 er vanlig
   # 50% == (lønn/1850)*1,5
   # 100% == (lønn/1850)*2
-  p ots50 = ((salary/1850.0)*1.5).round(2)
-  p ots100 = ((salary/1850.0)*2).round(2)
+  p ots50 = ((salary / 1850.0) * 1.5).round(2)
+  p ots100 = ((salary / 1850.0) * 2).round(2)
   # arbeidsdag = (AZ6*A$33+(AX6+AY6)*A$31)/5 == ((dayHours*ots50)+(nightHours*ots100)/5
   # helg = (AX6+AY6+AZ6)*A$31/5 == (dayHours+nightHours)*ots100/5
-  p workOvertime = (overtimeDayWorkHours*ots50+overtimeNightHours*ots100)/5
-  p weekendOvertime = overtimeWeekendHours*ots100/5
-  
-  print "Total: "
+  p workOvertime = (overtimeDayWorkHours * ots50 + overtimeNightHours * ots100) / 5
+  p weekendOvertime = overtimeWeekendHours * ots100 / 5
+
+  print 'Total: '
   (workOvertime + weekendOvertime).round(2)
 end
 
@@ -211,7 +213,7 @@ def calculate_earnings(worked, salary)
   p overtime = calculate_overtime(worked, salary)
 
   # endelig sum
-  p "Payout"
+  p 'Payout'
   p (compensation + overtime).round(2)
 end
 
@@ -219,7 +221,7 @@ def main
   periode = get_periode
   timecard = get_minwintid
   koststed, personalleder, salary = get_agresso
-  minutesWorked = parse_periode(periode, timecard, salary)
+  minutesWorked = parse_periode(periode[:periodes], timecard)
   calculate_earnings(minutesWorked, salary.to_i)
 end
 
