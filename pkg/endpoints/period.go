074f75aacd55f0c47c1a87f3550b1d516603f1d1
Kyrre Havik <kyrremann@gmail.com>
2022-11-04 15:39:24 +0100

    quickfix: fjerner update period for nå

--- pkg/endpoints/period.go
+++ pkg/endpoints/period.go
@@ -10,6 +10,11 @@ import (
 	"net/http"
 )
 
+const (
+	// TODO: Kopiert fra minwintid.go
+	vaktplanId = "vaktplanId"
+)
+
 func (h Handler) Period(w http.ResponseWriter, r *http.Request) {
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
@@ -39,40 +44,24 @@ func (h Handler) Period(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	oldPlan, err := h.Queries.GetPlan(h.Context, plan.ID)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error: %s", err), http.StatusInternalServerError)
-		h.Log.Error("Error when trying to check for earlier period", zap.Error(err))
-		return
-	}
+	err = h.Queries.CreatePlan(h.Context, gensql.CreatePlanParams{
+		ID:          plan.ID,
+		Ident:       plan.Ident,
+		Plan:        body,
+		PeriodBegin: plan.Begin,
+		PeriodEnd:   plan.End,
+	})
 
-	if oldPlan.Ident == "" {
-		err = h.Queries.CreatePlan(h.Context, gensql.CreatePlanParams{
-			ID:          plan.ID,
-			Ident:       plan.Ident,
-			Plan:        body,
-			PeriodBegin: plan.Begin,
-			PeriodEnd:   plan.End,
-		})
-	} else {
-		err = h.Queries.UpdatePlan(h.Context, gensql.UpdatePlanParams{
-			ID:          plan.ID,
-			Ident:       plan.Ident,
-			Plan:        body,
-			PeriodBegin: plan.Begin,
-			PeriodEnd:   plan.End,
-		})
-	}
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error: %s", err), http.StatusInternalServerError)
-		h.Log.Error("Error when trying to save period", zap.Error(err))
+		h.Log.Error("Error when trying to save period", zap.Error(err), zap.String(vaktplanId, plan.ID.String()))
 		return
 	}
 
 	h.Log.Info(fmt.Sprintf("Received period %v", plan.ID))
 	_, err = fmt.Fprint(w, "{\"message\":\"Period saved\"}\n")
 	if err != nil {
-		h.Log.Error("Error when returning success", zap.Error(err))
+		h.Log.Error("Error when returning success", zap.Error(err), zap.String(vaktplanId, plan.ID.String()))
 		return
 	}
 }
